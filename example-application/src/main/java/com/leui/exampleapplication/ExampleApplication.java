153f619e4f9d80fc2ed8ab1042340e0cfd9a3c31
Eui9179 <outdark97@gmail.com>
2025-12-11 13:00:07 +0900

    feat: use HelloPrint in app

--- example-application/src/main/java/com/leui/exampleapplication/ExampleApplication.java
+++ example-application/src/main/java/com/leui/exampleapplication/ExampleApplication.java
@@ -1,13 +1,18 @@
 package com.leui.exampleapplication;
 
+import com.leui.HelloPrinter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ExampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ExampleApplication.class, args);
+        ApplicationContext context = SpringApplication.run(ExampleApplication.class, args);
+        HelloPrinter printer = context.getBean(HelloPrinter.class);
+        printer.print();
     }
 
 }
