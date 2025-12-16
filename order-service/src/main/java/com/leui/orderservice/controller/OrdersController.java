dceade87074b67e52d836f3fa8dcfc8e4b3d988f
Eui9179 <outdark97@gmail.com>
2025-12-16 17:36:10 +0900

    feat: add order service to controller

--- order-service/src/main/java/com/leui/orderservice/controller/OrdersController.java
+++ order-service/src/main/java/com/leui/orderservice/controller/OrdersController.java
@@ -2,17 +2,23 @@ package com.leui.orderservice.controller;
 
 import com.leui.orderservice.dto.OrderRequest;
 import com.leui.orderservice.dto.OrderResponse;
+import com.leui.orderservice.service.OrdersService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/orders")
 public class OrdersController {
 
+    private final OrdersService ordersService;
+
     @PostMapping
     public ResponseEntity<OrderResponse> order(OrderRequest orderRequest) {
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(ordersService.createOrder(orderRequest));
     }
 }
--- order-service/src/main/java/com/leui/orderservice/service/OrdersServiceImpl.java
+++ order-service/src/main/java/com/leui/orderservice/service/OrdersServiceImpl.java
@@ -2,7 +2,9 @@ package com.leui.orderservice.service;
 
 import com.leui.orderservice.dto.OrderRequest;
 import com.leui.orderservice.dto.OrderResponse;
+import org.springframework.stereotype.Service;
 
+@Service
 public class OrdersServiceImpl implements OrdersService {
     @Override
     public OrderResponse createOrder(OrderRequest orderRequest) {
