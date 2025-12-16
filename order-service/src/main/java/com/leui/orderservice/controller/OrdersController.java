533efe5f9c6bda295bf28d614bfed1a1b5ce7a74
Eui9179 <outdark97@gmail.com>
2025-12-16 17:20:05 +0900

    feat: add order controller

--- order-service/src/main/java/com/leui/orderservice/controller/OrdersController.java
+++ order-service/src/main/java/com/leui/orderservice/controller/OrdersController.java
@@ -1,5 +1,8 @@
 package com.leui.orderservice.controller;
 
+import com.leui.orderservice.dto.OrderRequest;
+import com.leui.orderservice.dto.OrderResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,5 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/orders")
 public class OrdersController {
 
-
+    public ResponseEntity<OrderResponse> order(OrderRequest orderRequest) {
+        return ResponseEntity.ok().build();
+    }
 }
---
+++ order-service/src/main/java/com/leui/orderservice/dto/OrderResponse.java
@@ -0,0 +1,12 @@
+package com.leui.orderservice.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderResponse {
+    private Long orderId;
+}
