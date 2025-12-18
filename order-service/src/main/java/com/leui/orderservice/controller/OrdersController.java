40eaf0d1e60b13ada4728c422654f3d55376c965
Eui9179 <outdark97@gmail.com>
2025-12-18 13:54:25 +0900

    feat: set consumes and produces

--- order-service/src/main/java/com/leui/orderservice/controller/OrdersController.java
+++ order-service/src/main/java/com/leui/orderservice/controller/OrdersController.java
@@ -4,8 +4,10 @@ import com.leui.orderservice.dto.OrderRequest;
 import com.leui.orderservice.dto.OrderResponse;
 import com.leui.orderservice.service.OrdersService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +19,11 @@ public class OrdersController {
 
     private final OrdersService ordersService;
 
-    @PostMapping
-    public ResponseEntity<OrderResponse> order(OrderRequest orderRequest) {
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<OrderResponse> order(@RequestBody OrderRequest orderRequest) {
         return ResponseEntity.ok(ordersService.createOrder(orderRequest));
     }
 }
