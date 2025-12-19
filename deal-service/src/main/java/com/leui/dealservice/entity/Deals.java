b5e70731231b82e690e13d0ce105dcb1a0819217
Eui9179 <outdark97@gmail.com>
2025-12-19 11:30:53 +0900

    feat: add deal entity

---
+++ deal-service/src/main/java/com/leui/dealservice/entity/Deals.java
@@ -0,0 +1,47 @@
+package com.leui.dealservice.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SoftDelete;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@SoftDelete
+public class Deals extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long storeId;
+
+    @Size(max = 100, message = "The length of 'name' is exceeded")
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Size(max = 100, message = "The length of 'description' is exceeded")
+    @Column(nullable = false, length = 2000)
+    private String description;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int discountPrice;
+
+    @Column(nullable = false)
+    private int stockQuantity;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private DealsStatus dealsStatus;
+
+    @Column(nullable = false)
+    private LocalDateTime pickupEndTime;
+}
---
+++ deal-service/src/main/java/com/leui/dealservice/entity/DealsStatus.java
@@ -0,0 +1,5 @@
+package com.leui.dealservice.entity;
+
+public enum DealsStatus {
+    ON_SALE, SOLD_OUT, CLOSED
+}
