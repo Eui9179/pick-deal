65fe0a3025a2e1a30ab2d07e6645d121d3050acf
Eui9179 <outdark97@gmail.com>
2025-12-27 20:53:47 +0900

    feat: add store controller

---
+++ store-service/src/main/java/com/leui/storeservice/domain/store/controller/StoresController.java
@@ -0,0 +1,29 @@
+package com.leui.storeservice.domain.store.controller;
+
+import com.leui.storeservice.domain.store.dto.StoreInfoResponse;
+import com.leui.storeservice.domain.store.dto.StoreUpdateRequest;
+import com.leui.storeservice.domain.store.dto.StoresRequest;
+import com.leui.storeservice.domain.store.service.StoresService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/stores")
+public class StoresController {
+
+    private final StoresService storesService;
+
+    @GetMapping
+    public ResponseEntity<List<StoreInfoResponse>> getStores(StoresRequest request) {
+        return ResponseEntity.ok(storesService.getStores(request));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Long> updateStore(@PathVariable Long id, StoreUpdateRequest request) {
+        return ResponseEntity.ok(storesService.updateStore(id, request));
+    }
+}
