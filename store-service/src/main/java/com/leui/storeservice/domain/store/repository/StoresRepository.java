bf1ed6d2d0099f203aa325406432292e540bef5c
Eui9179 <outdark97@gmail.com>
2025-12-27 20:19:49 +0900

    feat: add store repository

--- store-service/src/main/java/com/leui/storeservice/domain/store/repository/StoresRepository.java
+++ store-service/src/main/java/com/leui/storeservice/domain/store/repository/StoresRepository.java
@@ -2,6 +2,8 @@ package com.leui.storeservice.domain.store.repository;
 
 import com.leui.storeservice.domain.store.entity.Stores;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StoresRepository extends JpaRepository<Stores, Long> {
 }
---
+++ store-service/src/main/java/com/leui/storeservice/domain/store/service/StoresService.java
@@ -0,0 +1,10 @@
+package com.leui.storeservice.domain.store.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StoresService {
+    
+
+
+}
