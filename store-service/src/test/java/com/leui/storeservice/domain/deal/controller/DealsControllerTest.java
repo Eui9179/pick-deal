ffc5e0436f216fdc7a2e2601e69b73712907fda5
Eui9179 <outdark97@gmail.com>
2025-12-29 11:18:57 +0900

    test: add deal controller test

--- store-service/src/test/java/com/leui/storeservice/domain/deal/controller/DealsControllerTest.java
+++ store-service/src/test/java/com/leui/storeservice/domain/deal/controller/DealsControllerTest.java
@@ -1,10 +1,16 @@
 package com.leui.storeservice.domain.deal.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.leui.storeservice.common.util.LocationUtils;
 import com.leui.storeservice.domain.deal.dto.DealsDetailResponse;
+import com.leui.storeservice.domain.deal.entity.DealCategory;
+import com.leui.storeservice.domain.deal.entity.Deals;
+import com.leui.storeservice.domain.deal.entity.DealsStatus;
 import com.leui.storeservice.domain.deal.repository.DealCategoryRepository;
 import com.leui.storeservice.domain.deal.repository.DealsRepository;
 import com.leui.storeservice.domain.deal.service.DealsService;
+import com.leui.storeservice.domain.store.entity.Stores;
+import com.leui.storeservice.domain.store.repository.StoresRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,18 +19,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Transactional
 @SpringBootTest
+@ActiveProfiles("postgres")
 @AutoConfigureMockMvc
 public class DealsControllerTest {
 
@@ -41,10 +50,14 @@ public class DealsControllerTest {
     DealCategoryRepository dealCategoryRepository;
 
     @Autowired
+    StoresRepository storesRepository;
+
+    @Autowired
     ObjectMapper objectMapper;
 
     @BeforeEach
-    void setUp() {
+    void setup() {
+        setupData();
     }
 
     @Test
@@ -66,5 +79,33 @@ public class DealsControllerTest {
                 .readValue(result.getResponse().getContentAsString(StandardCharsets.UTF_8), DealsDetailResponse.class);
 
         Assertions.assertThat(responseBody.id()).isEqualTo(id);
+    }
+
+    private void setupData() {
+        storesRepository.deleteAll();
+
+        Stores store = Stores.create(
+                "store_name",
+                LocationUtils.createPoint(1.1, 1.1),
+                "store_location",
+                "store_address",
+                LocalDateTime.now());
+
+        storesRepository.save(store);
+
+        DealCategory category = dealCategoryRepository.save(DealCategory.create("category"));
+
+        Deals deals = Deals.create(
+                store,
+                "deal_name",
+                "deal_description",
+                1,
+                1,
+                1,
+                DealsStatus.ON_SALE,
+                LocalDateTime.now(),
+                category);
+
+        dealsRepository.save(deals);
     }
 }
