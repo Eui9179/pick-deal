4987500450e9a38137fd724ecfaeaccb4e4a1c86
Eui9179 <outdark97@gmail.com>
2026-01-09 18:37:21 +0900

    feat: add decreasing stock

--- store-service/src/main/java/com/leui/storeservice/domain/deal/repository/DealRepository.java
+++ store-service/src/main/java/com/leui/storeservice/domain/deal/repository/DealRepository.java
@@ -2,7 +2,9 @@ package com.leui.storeservice.domain.deal.repository;
 
 import com.leui.storeservice.domain.deal.entity.Deal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -23,4 +25,14 @@ public interface DealRepository extends JpaRepository<Deal, Long> {
             where deal.id = :dealId
             """)
     Deal findByIdWithDiscountPolicy(Long dealId);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("""
+            update Deal d
+            set d.stockQuantity = d.stockQuantity -1
+            where d.id = :id
+            and d.stockQuantity > 0
+            """)
+    int decrease(@Param("id") Long id);
+
 }
