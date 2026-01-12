94b50af9c047b811233028092a3e1058f32ece20
Eui9179 <outdark97@gmail.com>
2026-01-12 21:42:36 +0900

    feat: add filter

--- api-gateway/src/main/java/com/leui/apigateway/config/GateWayRouteConfig.java
+++ api-gateway/src/main/java/com/leui/apigateway/config/GateWayRouteConfig.java
@@ -1,5 +1,8 @@
 package com.leui.apigateway.config;
 
+import com.leui.apigateway.filter.AuthenticationFilter;
+import jwt.JwtProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -9,14 +12,24 @@ import org.springframework.context.annotation.Configuration;
 public class GateWayRouteConfig {
 
     @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+    public JwtProvider jwtProvider(@Value("${auth.jwt.secret-key.}") String secret) {
+        return new JwtProvider(secret);
+    }
+
+    @Bean
+    public RouteLocator routeLocator(
+            RouteLocatorBuilder builder,
+            AuthenticationFilter authenticationFilter
+    ) {
         return builder.routes()
                 .route("store-service", r -> r
                         .path("/api/v1/stores/**")
+                        .filters(f -> f.filter(authenticationFilter.apply(new AuthenticationFilter.Config())))
                         .uri("lb://STORE-SERVICE")
                 )
                 .route("order-service", r -> r
                         .path("/api/v1/orders/**")
+                        .filters(f -> f.filter(authenticationFilter.apply(new AuthenticationFilter.Config())))
                         .uri("lb://ORDER-SERVICE")
                 )
                 .route("user-service", r -> r
