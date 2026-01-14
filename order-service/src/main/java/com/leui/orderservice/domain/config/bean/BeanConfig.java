23b5e7696a7f0f02f8081de92b8c951562f8f517
Eui9179 <outdark97@gmail.com>
2026-01-14 11:28:48 +0900

    feat: add kakao strategy

--- order-service/src/main/java/com/leui/orderservice/domain/config/bean/BeanConfig.java
+++ order-service/src/main/java/com/leui/orderservice/domain/config/bean/BeanConfig.java
@@ -1,6 +1,7 @@
 package com.leui.orderservice.domain.config.bean;
 
 import com.leui.orderservice.domain.payments.strategy.PaymentHandler;
+import com.leui.orderservice.domain.payments.strategy.kakao.KakaoPaymentStrategy;
 import com.leui.orderservice.domain.payments.strategy.toss.TossPaymentStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,9 @@ public class BeanConfig {
 
     @Bean
     public PaymentHandler paymentHandler() {
-        return new PaymentHandler(List.of(new TossPaymentStrategy()));
+        return new PaymentHandler(List.of(
+                new TossPaymentStrategy(),
+                new KakaoPaymentStrategy()
+        ));
     }
 }
