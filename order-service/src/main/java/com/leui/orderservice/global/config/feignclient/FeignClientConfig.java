4050ec1699e6ce5e731920e5e9d4f433d6f6bb6a
Eui9179 <outdark97@gmail.com>
2026-01-16 14:16:37 +0900

    feat: add FeignClientConfig

--- order-service/src/main/java/com/leui/orderservice/global/config/feignclient/FeignClientConfig.java
+++ order-service/src/main/java/com/leui/orderservice/global/config/feignclient/FeignClientConfig.java
@@ -1,8 +1,9 @@
 package com.leui.orderservice.global.config.feignclient;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-
+@EnableFeignClients
 public class FeignClientConfig {
 }
