dc394679c52931ab64528bbdee2c21ec481d24fa
Eui9179 <outdark97@gmail.com>
2026-01-23 01:12:12 +0900

    feat: add deal stock reservation

--- order-service/src/main/java/com/leui/orderservice/global/bean/BeanConfig.java
+++ order-service/src/main/java/com/leui/orderservice/global/bean/BeanConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import redis.RedisRepository;
 
 import java.util.List;
 
@@ -17,12 +18,6 @@ public class BeanConfig {
     @Bean
     public PaymentProviderHandler paymentHandler(TossPaymentStrategy tossPayment) {
         return new PaymentProviderHandler(List.of(tossPayment));
-    }
-
-
-    @Bean
-    public StringRedisTemplate redisTemplate(RedisConnectionFactory factory) {
-        return new StringRedisTemplate(factory);
     }
 
 }
