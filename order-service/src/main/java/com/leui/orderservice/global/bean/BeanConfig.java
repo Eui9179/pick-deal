a7a82e5a88a9cf5fa05364f10cc3fdb25423e426
Eui9179 <outdark97@gmail.com>
2026-01-22 22:27:21 +0900

    feat: add redis repository

--- order-service/src/main/java/com/leui/orderservice/global/bean/BeanConfig.java
+++ order-service/src/main/java/com/leui/orderservice/global/bean/BeanConfig.java
@@ -1,12 +1,13 @@
 package com.leui.orderservice.global.bean;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.leui.orderservice.domain.order.service.OrderService;
 import com.leui.orderservice.domain.payments.provider.PaymentProviderHandler;
 import com.leui.orderservice.domain.payments.provider.toss.strategy.TossPaymentStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import redis.RedisRepository;
 
 import java.util.List;
 
@@ -16,6 +17,12 @@ public class BeanConfig {
     @Bean
     public PaymentProviderHandler paymentHandler(TossPaymentStrategy tossPayment) {
         return new PaymentProviderHandler(List.of(tossPayment));
+    }
+
+
+    @Bean
+    public StringRedisTemplate redisTemplate(RedisConnectionFactory factory) {
+        return new StringRedisTemplate(factory);
     }
 
 }
